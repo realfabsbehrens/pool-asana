22d92040566c344d6cf83d308271bf0e8bf3096c
fabsbehrens <moin@fabsbehrens.de>
2024-01-28 18:15:45 +0100

    removed headline

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
-import Nav from "@/components/Nav"
-import Table from "@/components/table"
-export default function Home() {
-  return (
-    <main className="p-24 ">
- <Nav />
-   
-<section className="py-24 flex flex-col items-center gap-8"> 
+import Nav from "@/components/Nav";
+import Table from "@/components/table";
 
-<div className="left">
-<h1 className="text-4xl left-0 font-bold mb-2">poool x asana</h1>
-<p className="text-1xl left text-muted-foreground">
- auf dieser seite stesten wir eine beidseitige verbindung von poool und asana. 
- </p>
-</div>
-<Table />
-</section>
- </main>
+export default async function Home() {
+  return (
+    <main className=" p-24 ">
+      <Nav />
+
+      <section className="py-24 flex flex-col  justify-center  justify-items-center gap-8">
+        <Table />
+      </section>
+    </main>
   );
 }

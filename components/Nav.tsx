1a77d034b6bd478a7e9eb951dd094984f12b92f8
fabsbehrens <moin@fabsbehrens.de>
2024-01-28 18:16:40 +0100

    added database

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,25 +1,27 @@
-"use client"
+"use client";
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./ui/toggle-mode";
-
 
 export default function Nav() {
   return (
-<header>
-<nav>
-<ul className="flex items-center justify-between">
-    <li>
-<p className="text-6xl font-bold bg-gradient-to-r from-pink-500 to-red-400 bg-clip-text text-transparent"> pool x asana</p>
-      </li>
+    <header>
+      <nav>
+        <ul className="flex items-center justify-between">
+          <li>
+            <p className="text-6xl font-bold bg-gradient-to-r from-pink-500 to-red-400 bg-clip-text text-transparent">
+              {" "}
+              pool x asana
+            </p>
+          </li>
 
-
-      <li className="flex items-center justify-between gap-4">
-<Button variant={"secondary"}>Asana Board</Button>
-<ModeToggle></ModeToggle>
-      </li>
-</ul>
-</nav>
-</header>
+          <li className="flex items-center justify-between gap-4">
+            <Button variant={"secondary"}>Datenbank</Button>
+            <Button variant={"secondary"}>Asana Board</Button>
+            <ModeToggle></ModeToggle>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }

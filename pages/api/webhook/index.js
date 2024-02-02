8f4839e81a58c7e039357fda1cd68c4422f22ee5
fabsbehrens <moin@fabsbehrens.de>
2024-02-02 16:49:12 +0100

    webhook test 5

--- pages/api/webhook/index.js
+++ pages/api/webhook/index.js
@@ -10,50 +10,56 @@ const accessToken = process.env.ASANAKEY;
 let secret = "";
 
 export default async function handler(req, res) {
-  if (req.headers["x-hook-secret"]) {
-    console.log("This is a new webhook");
-    secret = req.headers["x-hook-secret"];
+  try {
+    if (req.headers["x-hook-secret"]) {
+      console.log("This is a new webhook");
+      secret = req.headers["x-hook-secret"];
 
-    res.setHeader("X-Hook-Secret", secret);
-    res.status(200).end();
-  } else if (req.headers["x-hook-signature"]) {
-    const computedSignature = createHmac("SHA256", secret)
-      .update(JSON.stringify(req.body))
-      .digest("hex");
-
-    if (
-      !crypto.timingSafeEqual(
-        Buffer.from(req.headers["x-hook-signature"]),
-        Buffer.from(computedSignature)
-      )
-    ) {
-      // Fail
-      res.status(401).end();
-    } else {
-      // Success
+      res.setHeader("X-Hook-Secret", secret);
       res.status(200).end();
-      console.log(`Events on ${Date()}:`);
-      console.log(req.body.events);
-      req.body.events.forEach((event) => {
-        fetchTask(event.resource.gid);
-      });
+    } else if (req.headers["x-hook-signature"]) {
+      const computedSignature = createHmac("SHA256", secret || "")
+        .update(JSON.stringify(req.body))
+        .digest("hex");
+
+      if (
+        !crypto.timingSafeEqual(
+          Buffer.from(req.headers["x-hook-signature"]),
+          Buffer.from(computedSignature)
+        )
+      ) {
+        // Fail
+        res.status(401).end();
+      } else {
+        // Success
+        res.status(200).end();
+        console.log(`Events on ${Date()}:`);
+        console.log(req.body.events);
+        req.body.events.forEach((event) => {
+          fetchTask(event.resource.gid);
+        });
+      }
+    } else {
+      console.error("Invalid request");
+      res.status(400).end();
     }
-  } else {
-    console.error("Something went wrong!");
+  } catch (error) {
+    console.error("Error in handler:", error.message);
     res.status(500).end();
   }
 }
 
 async function fetchTask(taskId) {
-  const url = `https://app.asana.com/api/1.0/tasks/${taskId}`;
-  const headers = {
-    Accept: "application/json",
-    Authorization: `Bearer ${accessToken}`,
-  };
   try {
+    const url = `https://app.asana.com/api/1.0/tasks/${taskId}`;
+    const headers = {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    };
+
     const response = await axios.get(url, { headers });
     console.log(response.data.data);
   } catch (err) {
-    console.log(err);
+    console.error("Error in fetchTask:", err.message);
   }
 }

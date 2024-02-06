84bfe49afd717abe53f876c0720a25e41af57897
fabsbehrens <moin@fabsbehrens.de>
2024-02-06 18:42:33 +0100

    webhook delete

--- pages/api/webhooks/delete/index.js
+++ pages/api/webhooks/delete/index.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
         // Success
         res.status(200).end();
         let asanaGID = req.body.events[0].resource.gid;
-        deletetask(asanaGID);
+        deleteTask(asanaGID);
       }
     } else {
       console.error("Invalid request");

465a86a8085f2720e326985c4c3e39d3360b2478
fabsbehrens <moin@fabsbehrens.de>
2024-02-10 15:19:30 +0100

    neue mongo db verbindung

--- pages/api/webhooks/delete/index.tsx
+++ pages/api/webhooks/delete/index.tsx
@@ -1,7 +1,24 @@
 // pages/api/webhook.js
 import crypto from "crypto";
 import { createHmac } from "crypto";
-import { DeleteTask } from "@/lib/mongodbneu";
+
+const DeleteTask = async (asanaGid: string) => {
+  try {
+    let response = await fetch(
+      "https://pool-asana.vercel.app/api/tasks/delete?asanaGid=" + asanaGid,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    response = await response.json();
+  } catch (error) {
+    console.log("An error occurred while deleting ", error);
+  }
+};
 
 // Replace 'YOUR_ACCESS_TOKEN' with your Asana Personal Access Token
 const accessToken = process.env.ASANAKEY;
@@ -33,7 +50,7 @@ export default async function handler(req, res) {
         // Success
         res.status(200).end();
         if (req.body.events[0]) {
-          DeleteTask("1206569073560664");
+          await DeleteTask("1206569073560664");
         }
       }
     } else {

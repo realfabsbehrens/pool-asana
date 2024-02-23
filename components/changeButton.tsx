c8b89ad36443872e5bdab514569c81978d38cb8c
fabsbehrens <moin@fabsbehrens.de>
2024-02-23 16:06:03 +0100

    modal

--- components/changeButton.tsx
+++ components/changeButton.tsx
@@ -47,7 +47,7 @@ export default function changeButton({ ticket }) {
           </div>
         </div>
 
-        <div className="flex items-center  border-b-2space-x-2 my-20 ml-80 mr-80"></div>
+        <div className="flex items-center  border-b-2 space-x-2 my-5 ml-80 mr-80"></div>
 
         <div className="flex items-center  space-x-2">
           <div className="grid flex-1 gap-2">

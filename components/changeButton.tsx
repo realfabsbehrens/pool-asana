3a6acfb1ea8ff1f64a6e869fc7eeba88647157df
fabsbehrens <moin@fabsbehrens.de>
2024-02-23 15:50:16 +0100

    konfig button

--- components/changeButton.tsx
+++ components/changeButton.tsx
@@ -73,7 +73,7 @@ export default function changeButton({ ticket }) {
           </div>
         </div>
 
-        <DialogFooter className="sm:justify-start">
+        <DialogFooter className="sm:justify-start justify-between">
           <DialogClose asChild>
             <Button type="button" variant="secondary">
               schliessen

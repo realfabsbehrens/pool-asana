263d88483b8e490e07f86196d2aa274cf0ae42f6
fabsbehrens <moin@fabsbehrens.de>
2024-02-27 20:16:47 +0100

    changed wbehook insert name

--- lib/asana/index.tsx
+++ lib/asana/index.tsx
@@ -47,7 +47,6 @@ export async function getAndInsertTask(asanaGID) {
     const response = await getAsanaTask(asanaGID);
 
     if (response.data.gid) {
-      const assignee = response.data.assignee.name;
       const status =
         response.data.custom_fields[0]?.enum_value?.name ?? "DefaultStatus";
       const kunde =
@@ -59,7 +58,7 @@ export async function getAndInsertTask(asanaGID) {
 
       let taskData = {
         name: name,
-        assignee: assignee,
+        assignee: "fabian behrens",
         workspace: "Workplace",
         asanaGID: asanaGID,
         nummer: nummer,
